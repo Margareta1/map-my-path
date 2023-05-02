8b7d315d466c73a809060cd5d193042a3660b1b0
Jovana Obradovic <jobrado@racunarstvo.hr>
2023-05-02 22:56:00 +0200

    -navbar fix - logo center -added edituser page -added about and privacy page

--- MapMyPath/MapMyPathWeb/Controllers/UserController.cs
+++ MapMyPath/MapMyPathWeb/Controllers/UserController.cs
@@ -12,5 +12,14 @@ namespace MapMyPathWeb.Controllers
         {
             return View();
         }
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteUser()
+        {
+            return View();
+        }
     }
 }
