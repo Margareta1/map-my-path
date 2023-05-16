e81dcd380a438e9075f8bacccb8c2223df5a2902
Margareta <margareta.zeko@gmail.com>
2023-05-16 16:51:02 +0200

    added reference to project

---
+++ MapMyPath/MapMyPathApi/Services/LocalService.cs
@@ -0,0 +1,19 @@
+﻿using MapMyPathApi.Controllers;
+
+namespace MapMyPathApi.Services
+{
+    public class LocalService
+    {
+        private UserController userController;
+        public LocalService()
+        {
+            userController = new UserController();
+        }
+
+        private static void DoSomething()
+        {
+
+        }
+
+    }
+}
