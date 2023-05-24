f8a912a5fb8f9bb113c0e6e1a36da26a143de3a6
Margareta <margareta.zeko@gmail.com>
2023-05-24 12:41:10 +0200

    added tests

--- MapMyPath/MapMyPathWebTests/HomeControllerTests.cs
+++ MapMyPath/MapMyPathWebTests/HomeControllerTests.cs
@@ -18,9 +18,43 @@ namespace MapMyPathWebTests
         public void Setup()
         {
             controller = new HomeController(new TestLogger<HomeController>());
+
+        }
+
+        [Test]
+        public void Index_ReturnsViewResult()
+        {
             var result = controller.Index();
 
             Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(result);
+        }       
+        [Test]
+        public void Privacy_ReturnsViewResult()
+        {
+            var result = controller.Privacy();
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(result);
+
+        }
+        [Test]
+        public void About_ReturnsViewResult()
+        {
+            var result = controller.About();
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsNotNull(result);
+
+        }
+        [Test]
+        public void LiveFeed_ReturnsViewResult()
+        {
+            var result = controller.LiveFeed();
+
+            Assert.IsInstanceOf<ViewResult>(result); 
+            Assert.IsNotNull(result);
+
         }
 
     }
