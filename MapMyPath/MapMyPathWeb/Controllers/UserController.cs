bd42d302169411f3356d725f89d332fc0c9c3431
Jovana Obradovic <jobrado@racunarstvo.hr>
2023-05-29 21:11:51 +0200

    registration fix

--- MapMyPath/MapMyPathWeb/Controllers/UserController.cs
+++ MapMyPath/MapMyPathWeb/Controllers/UserController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MapMyPathLib;
+using MapMyPathWeb.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MapMyPathWeb.Controllers
 {
     public class UserController : Controller
     {
+        private AccountService accountService;
+        public UserController()
+        {
+            accountService = new AccountService();
+        }
+
         public IActionResult SignIn()
         {
+           
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (accountService.ValidateUser(appUser.UserName, appUser.Password)) { 
+
+               
+                }
+            }
+            ModelState.AddModelError("", "invalid Username or Password");
             return View();
         }
 
@@ -13,13 +36,34 @@ namespace MapMyPathWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(AppUser appUser )
+        {
+            if (ModelState.IsValid)
+            {
+               
+                accountService.AddUser(appUser.UserName, appUser.Password, appUser.FirstName, appUser.LastName);
+                return RedirectToAction("SignIn");
+            }
+            return View();
+        }
+
         public IActionResult EditUser()
         {
             return View();
         }
 
+
         public IActionResult DeleteUser()
         {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteUser(AppUser user)
+        {
+            accountService.DeleteUser(user.UserName);
             return View();
         }
     }
--- MapMyPath/MapMyPathWeb/Program.cs
+++ MapMyPath/MapMyPathWeb/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
