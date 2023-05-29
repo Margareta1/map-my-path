96d776a01b3fdbaddd5f007723b2be248be3b2dd
Jovana Obradovic <jobrado@racunarstvo.hr>
2023-05-29 21:11:51 +0200

    registration fix

--- MapMyPath/MapMyPathLib/AppUser.cs
+++ MapMyPath/MapMyPathLib/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,17 @@ namespace MapMyPathLib
         public Guid Id { get; set; }
         [DisplayName("First name:")]
         public string FirstName { get; set; }
+      
+
         [DisplayName("Last name:")]
         public string LastName { get; set; }
         [DisplayName("Username:")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string UserName { get; set; }
         [DisplayName("Password:")]
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
       
         public int IsDeleted { get; set; } = 0;
